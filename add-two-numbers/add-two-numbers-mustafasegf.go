1aef0230c355dea74914c8b87ec8abb6f0c4e227
Mustafa Zaki Assagaf <--get>
2022-07-04 02:54:41 +0700

    feat: added problem 2. add two numbers

---
+++ add-two-numbers/add-two-numbers-mustafasegf.go
@@ -0,0 +1,36 @@
+package addTwoNumbers
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carry := 0
+	ans := &ListNode{}
+	p := ans
+
+	for l1 != nil || l2 != nil {
+		t := &ListNode{Val: carry}
+		if l1 != nil {
+			t.Val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			t.Val += l2.Val
+			l2 = l2.Next
+		}
+		carry = t.Val / 10
+		t.Val %= 10
+		p.Next = t
+		p = p.Next
+	}
+
+	if carry == 1 {
+		p.Next = &ListNode{Val: carry}
+	}
+
+	return ans.Next
+}
