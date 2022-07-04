d3ff92cece54490533c5cb91dc4b7fb5b34de677
Mustafa Zaki Assagaf <--get>
2022-07-04 19:31:47 +0700

    feat: added problem 217. contains duplicate

---
+++ contains-duplicate/contains-duplicate-mustafasegf.go
@@ -0,0 +1,14 @@
+package containsduplicate
+
+func containsDuplicate(nums []int) bool {
+	set := map[int]bool{}
+
+	for _, n := range nums {
+		if _, ok := set[n]; ok {
+			return true
+		}
+
+		set[n] = true
+	}
+	return false
+}
